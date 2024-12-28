d177912d7ee0642bb07ad12edc082035d759a96e
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-12-28 20:17:55 +0100

    Cleaning the server section

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,1 @@
-export default [
-  {
-    method: 'GET',
-    path: '/',
-    // name of the controller file & the method.
-    handler: 'controller.index',
-    config: {
-      policies: [],
-    },
-  },
-];
+export default [];
